0a5a6cb3e581748f3503c6ea2b055fd745c3e838
Genearendar <Genearendar@gmail.com>
2022-07-24 18:01:11 +1200

    Added event handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import RobotsList from "./Robots-list"
+import Search from "./Search"
 import { robots } from "./robots.js"
 
 function App() {
-  return <RobotsList robots={robots} />
+  const [search, setSearch] = useState("")
+  const [filteredRobots, setFilteredRobots] = useState(robots)
+
+  function handleSearch(event) {
+    const { value } = event.target
+    setSearch(value)
+  }
+
+  useEffect(() => {
+    search.length
+      ? setFilteredRobots(() =>
+          robots.filter((robot) => {
+            const values = Object.values(robot)
+            const regex = new RegExp(search, "i")
+            console.log(regex)
+            return values.some((val) => {
+              let result = regex.test(val)
+              return result
+            })
+          })
+        )
+      : setFilteredRobots(robots)
+  }, [search])
+
+  return (
+    <div className="main-container">
+      <h1>My RoboFriends</h1>
+      <Search value={search} handleSearch={handleSearch} />
+      <RobotsList robots={filteredRobots} />
+    </div>
+  )
 }
 
 export default App
---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Search({ value, handleSearch }) {
+  return (
+    <div className="search-bar">
+      <input
+        className="robots-search"
+        placeholder="Serch robots"
+        onChange={handleSearch}
+        value={value}
+      ></input>
+    </div>
+  )
+}
