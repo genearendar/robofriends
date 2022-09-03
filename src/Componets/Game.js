26bb95f8bf7ffb1b7221442e9fc4293f1501ed80
Genearendar <Genearendar@gmail.com>
2022-09-03 22:43:03 +1200

    Added high score functionality with local storage

--- src/Componets/Game.js
+++ src/Componets/Game.js
@@ -7,6 +7,19 @@ export default function Game({ robots }) {
     [...robots].sort(() => Math.random() - 0.5)
   );
   let [currentScore, setCurrentScore] = useState(0);
+  let [highScore, setHighScore] = useState(
+    localStorage.getItem("highScore") !== null
+      ? Number(localStorage.getItem("highScore"))
+      : 0
+  );
+
+  if (currentScore > highScore) {
+    setHighScore(currentScore);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("highScore", highScore.toString());
+  }, [highScore]);
 
   function selectRobot(event) {
     if (event.target.isSelected) {
@@ -16,8 +29,9 @@ export default function Game({ robots }) {
       return;
     }
     event.target.isSelected = true;
+
     setCurrentScore((prevScore) => prevScore + 1);
-    console.log(currentScore);
+
     shuffleRobots();
 
     function shuffleRobots() {
@@ -27,7 +41,7 @@ export default function Game({ robots }) {
 
   return (
     <main>
-      <Score currentScore={currentScore} />
+      <Score currentScore={currentScore} highScore={highScore} />
       <RobotsList robots={shuffledRobots} selectRobot={selectRobot} />
     </main>
   );
--- src/Componets/Score.js
+++ src/Componets/Score.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-export default function Score({ currentScore }) {
+export default function Score({ currentScore, highScore }) {
   return (
     <div className="score-panel d-flex">
       <div>
         <span>Score</span>
-        <span>0</span>
+        <span>{currentScore}</span>
       </div>
       <div>
         <span>High Score</span>
-        <span>{currentScore}</span>
+        <span>{highScore}</span>
       </div>
     </div>
   );
