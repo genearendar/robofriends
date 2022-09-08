3558a6cdce02dd923e6847c6089376866df29d81
Genearendar <Genearendar@gmail.com>
2022-09-08 20:36:13 +1200

    Added conditionals in Robots-list and Robots-card to prevent game functions in catalogue, changed card appearance in the game

--- src/Componets/Robot-card.js
+++ src/Componets/Robot-card.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 export default function RobotCard({ name, email, id, selectRobot }) {
+  function handleClick() {
+    if (selectRobot) {
+      selectRobot(id);
+    }
+  }
   return (
-    <div className="robot__card" onClick={() => selectRobot(id)}>
+    <div className="robot__card" onClick={handleClick}>
       <img src={`https://robohash.org/${id}`} alt="robot" />
       <h2>{name}</h2>
       <p>{email}</p>
--- src/Componets/Robots-list.js
+++ src/Componets/Robots-list.js
@@ -2,10 +2,13 @@ import React from "react";
 import RobotCard from "./Robot-card";
 import "../CSS/robots.css";
 
-// Shuffling cards for the game
-
 export default function RobotsList({ robots, selectRobot }) {
   const robotCards = robots.map((robot) => {
+    if (selectRobot) {
+      return (
+        <RobotCard id={robot.id} key={robot.id} selectRobot={selectRobot} />
+      );
+    }
     return (
       <RobotCard
         id={robot.id}
